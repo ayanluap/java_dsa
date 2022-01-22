5a57715f2f531effc8b1fd84c03dc59bc117d6af
ayanluap <ayanluap@gmail.com>
2022-01-22 12:26:17 +0530

    OOPS in JAVA learned

---
+++ beginning/OOPS.java
@@ -0,0 +1,66 @@
+package beginning;
+
+
+class Student{
+    String name;
+    int age;
+    
+    ///// POLYMORPHISM (2 types -> 01. function overloading, 02. function overriding)
+
+    // Function overloading [Compile time polymorphism]
+    Student(){
+        System.out.println("no args passed");
+    }
+
+    Student(String name){
+        System.out.println("name : "+name);
+    }
+
+    Student(int age){
+        System.out.println("age : "+age);
+    }
+
+    Student(String name, int age){
+        this.name=name;
+        this.age=age;
+        System.out.println("name & age : "+name+" "+age);
+    }
+}
+
+///// INHERITANCE [increases reusability] 
+
+// 4 types of INHERITANCE in JAVA    [in CPP there are 5 types extra one is MULTIPLE LEVEL INHERITANCE]
+// 01. Single level
+// 02. Multilevel level
+// 03. Heirarchial
+// 04. Hybrid
+
+// base-class OR parent-class
+class Shape{
+    String color;
+}
+// child-class OR sub-class
+class Triangle extends Shape{
+    
+}
+
+/////   ACCESS MODIFIERS
+
+// 01. public       (can be accessed by any Class or Package - thats why main func is always written in public class so  that it can always be accessible )
+
+// 02. default      (when we dont provide any access modifier its auto. set to default can be accessed within a Package but not bt other Packages)
+
+// 03. protected    (can be accessed by ANY CLASSES OF OWN PACKAGE and ONLY SUB-CLASSES OF OTHER PACKAGES)
+
+// 04. private      (can be accessed only inside the class. If need to modify or store Private attributes/methods we need to setup GETTERS & SETTERS)
+
+
+
+public class OOPS {
+    public static void main(String args[]){
+        Student s1=new Student("ayan",23);
+
+        Triangle t1=new Triangle();
+        t1.color="red";
+    }
+}
--- beginning/Sorting.java
+++ beginning/Sorting.java
@@ -1,5 +1,55 @@
 package beginning;
 
 public class Sorting {
-    
+    public static void main(String args[]){
+        int arr[]={7,8,3,1,2};
+
+        ///// BUBBLE SORT ~ O(n^2)
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j] > arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.print("\nBubble Sort : ");
+        for(int x: arr) System.out.print(x+" ");
+        System.out.println("\n");
+
+        ///// SELECTION SORT ~ O(n^2)
+        for(int i=0; i<arr.length-1; i++){
+            int min=i;
+            for(int j=i+1; j<arr.length-1; j++){
+                if(arr[min] > arr[j]) {
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        System.out.print("Selection Sort : ");
+        for(int x: arr) System.out.print(x+" ");
+        System.out.println("\n");
+
+        ///// INSERTION SORT ~ O(n^2)
+        for(int i=1; i<arr.length; i++){
+            int elem=arr[i];
+            int j=i-1;
+            while(j>=0 && elem < arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+
+            // placement of element
+            arr[j+1]=elem;
+        }
+        System.out.print("Intertion Sort : ");
+        for(int x: arr) System.out.print(x+" ");
+        System.out.println("\n");
+
+
+    }
 }
