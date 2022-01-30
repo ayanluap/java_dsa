58b08f4ae81d0be775e0b756b9092d85c6c87d5e
ayanluap <ayanluap@gmail.com>
2022-01-30 15:35:26 +0530

    min cost maze solved [DP]

--- level_01/L6_DP/Q35_MinCostInMazeTraversal.java
+++ level_01/L6_DP/Q35_MinCostInMazeTraversal.java
@@ -10,11 +10,38 @@ import java.util.*;
 // 7. You are required to traverse through the matrix and print the cost of path which is least costly.
 
 public class Q35_MinCostInMazeTraversal {
-    public static void mail(String args[]){
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int m=sc.nextInt();
         int maze[][]=new int[n][m];
-        
+
+        for(int r=0; r<n; r++){
+            for(int c=0; c<m; c++){
+                maze[r][c]=sc.nextInt();
+            }
+        }
+        System.out.println(minCost(maze, n,m)); 
+    }
+
+    // using memoization
+    public static int minCost(int maze[][], int row, int col){
+        Integer dp[][]=new Integer[row][col];
+        dp[row-1][col-1]=maze[row-1][col-1];
+
+        for(int colNum=col-2; colNum>=0; colNum--){
+            dp[row-1][colNum]=maze[row-1][colNum]+dp[row-1][colNum+1];
+        }
+        for(int rowNum=row-2; rowNum>=0; rowNum--){
+            dp[rowNum][col-1]=maze[rowNum][col-1]+dp[rowNum+1][col-1];
+        }
+
+        for(int r=row-2; r>=0; r--){
+            for(int c=col-2; c>=0; c--){
+                dp[r][c]=Math.min(dp[r+1][c], dp[r][c+1])+maze[r][c];
+            }
+        }
+
+        return dp[0][0];
     }
 }
