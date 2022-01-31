533c99bfbc4c3e01d6a5fd627ce77eff4b060c60
ayanluap <ayanluap@gmail.com>
2022-01-31 19:53:56 +0530

    solved by Tabulative approach Recur. approach remains

---
+++ level_01/L6_DP/Q36_Goldmine.java
@@ -0,0 +1,81 @@
+package level_01.L6_DP;
+import java.util.*;
+
+// 1. You are given a number n, representing the number of rows.
+// 2. You are given a number m, representing the number of columns.
+// 3. You are given n*m numbers, representing elements of 2d array a, which represents a gold mine.
+// 4. You are standing in front of left wall and are supposed to dig to the right wall. You can start from any row in the left wall.
+// 5. You are allowed to move 1 cell right-up (d1), 1 cell right (d2) or 1 cell right-down(d3).
+
+public class Q36_Goldmine {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int maze[][]=new int[n][m];
+
+        for(int r=0; r<n; r++){
+            for(int c=0; c<m; c++){
+                maze[r][c]=sc.nextInt();
+            }
+        }
+
+        // Recursion
+        Integer dp[][]=new Integer[n][m];
+        
+        int max=goldmineRec(maze, 0, 0, dp, Integer.MIN_VALUE);
+        System.out.println(max);
+        System.out.println(goldmine(maze));
+    }
+
+    // using recursion
+    // public static int goldmineRec(int maze[][], int row, int col, Integer dp[][], int ans){
+    //     if(col==maze[0].length-1 && row<maze.length) return dp[row][col]=maze[row][col];
+
+    //     if(dp[row][col]!=null) return dp[row][col];
+        
+    //     for(int i=0; i<maze.length; i++){
+    //         int rightUp=0;
+    //         int rightDown=0;
+    //         if(row>0) rightUp= goldmineRec(maze, row-1, col+1, dp, ans);
+    //         int right= goldmineRec(maze, row, col+1, dp, ans);
+    //         if(row<maze.length-1) rightDown= goldmineRec(maze, row+1, col+1, dp, ans);
+
+    //         dp[row][col]=maze[row][col]+Math.max(right, Math.max(rightUp,rightDown));
+    //         if (dp[row][col] > ans) ans=dp[row][col];
+    //     }
+
+    //     return ans;
+    // }
+
+    
+
+    // using tabulation
+    public static int goldmine(int maze[][]){
+        int row=maze.length-1;
+        int col=maze[0].length-1;
+
+        int dp[][]=new int[row+1][col+1];
+        for(int r=0; r<=row; r++){
+            dp[r][col]=maze[r][col];
+        }
+
+        for(int c=col-1; c>=0; c--){
+            for(int r=0; r<=row; r++){
+                if(r==0) {
+                    dp[r][c]=maze[r][c]+Math.max(dp[r][c+1], dp[r+1][c+1]);
+                }else if(r==row){
+                    dp[r][c]=maze[r][c]+Math.max(dp[r][c+1], dp[r-1][c+1]);
+                }else{
+                    dp[r][c]=maze[r][c]+Math.max(dp[r][c+1], Math.max(dp[r+1][c+1], dp[r-1][c+1]));
+                }
+            }
+        }
+
+        int max=Integer.MIN_VALUE;
+        for(int r=0; r<=row; r++){
+            max=Math.max(max, dp[r][0]);
+        }
+        return max;
+    }
+}
