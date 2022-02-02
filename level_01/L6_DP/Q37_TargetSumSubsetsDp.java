7b3bf008ad526af12d0396084c263a8d7b5964dc
ayanluap <ayanluap@gmail.com>
2022-02-02 19:33:40 +0530

    target sum (Knapsack)

--- level_01/L6_DP/Q37_TargetSumSubsetsDp.java
+++ level_01/L6_DP/Q37_TargetSumSubsetsDp.java
@@ -17,10 +17,47 @@ public class Q37_TargetSumSubsetsDp {
         }
         int tar=sc.nextInt();
 
-        // targetSum(arr, tar, new int[]);
+        Boolean dp[][]=new Boolean[n+1][tar+1];
+        // System.out.println(targetSum(arr, n, tar, dp));
+
+        System.out.println(targetSumTab(arr, n, tar, dp));
+        
     }
 
-    public static void targetSum(int arr[], int tar, int qb[]){
+    // Using 0-1 Knapsack recursion
+    public static boolean targetSum(int arr[], int n, int tar, Boolean dp[][]){
+        if(n==0 && tar==0) return true;
+        else if(tar<0 || n<=0) return false;
 
+        if(dp[n][tar]!=null) return dp[n][tar];
+
+        if(arr[n-1]<=tar){
+            dp[n][tar]= targetSum(arr, n-1, tar-arr[n-1], dp) || targetSum(arr, n-1, tar, dp);
+            
+        }else{
+            dp[n][tar]=targetSum(arr, n-1, tar, dp);
+        }
+
+        return dp[n][tar];
+    }
+
+    // using tabulation
+    public static boolean targetSumTab(int arr[], int n, int tar, Boolean dp[][]){
+
+        for(int j=1; j<=tar; j++) dp[0][j]=false;
+        for(int i=1; i<=n; i++) dp[i][0]=true;
+        dp[0][0]=true;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=tar; j++){
+
+                if(arr[i-1] <= j){
+                    dp[i][j]= dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][tar];
     }
 }
