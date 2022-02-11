287471fcc09b671c751585a7223641972aa0df55
ayanluap <ayanluap@gmail.com>
2022-02-11 19:34:46 +0530

    TnC q2

--- level_01/L7_TimeAndSpaceComplexity/Q47_Sort012.java
+++ level_01/L7_TimeAndSpaceComplexity/Q47_Sort012.java
@@ -5,6 +5,47 @@ import java.util.*;
 // 2. You have to sort the given array in increasing order and in linear time.
 
 public class Q47_Sort012 {
-    // static 
+    public static void sort012(int[] arr){
+        //write your code here
+        int i=0; int k=i; int j=arr.length-1;
+        
+        while(k<j){
+            if(arr[k]==0){
+                swap(arr, i, k);
+                i++;
+                k++;
+            }else if(arr[k]==1){
+                k++;
+            }else if(arr[k]==2){
+                swap(arr, k, j);
+                j--;
+            }
+        }
+        
+      }
+    
+      // used for swapping ith and jth elements of array
+      public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping index " + i + " and index " + j);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    
+      public static void print(int[] arr){
+        for(int i = 0 ; i < arr.length; i++){
+          System.out.println(arr[i]);
+        }
+      }
+      public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i < n; i++){
+          arr[i] = scn.nextInt();
+        }
+        sort012(arr);
+        print(arr);
+      }
     
 }
---
+++ level_01/L7_TimeAndSpaceComplexity/Q48_Sort012.java
@@ -0,0 +1,5 @@
+package L7_TimeAndSpaceComplexity;
+
+public class Q48_Sort012 {
+    
+}
