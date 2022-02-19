b30a8b6c075d465d06541998a89d0200ff364cce
ayanluap <ayanluap@gmail.com>
2022-02-19 19:14:11 +0530

    LL 7th Q done (92th question/31 day)

--- level_01/L9_LinkedList/Q90_AddFirstInLinkedList.java
+++ level_01/L9_LinkedList/Q90_AddFirstInLinkedList.java
@@ -5,16 +5,16 @@ import java.io.*;
 // 2. Here is a list of existing functions:
 //     2.1 addLast - adds a new element with given value to the end of Linked List
 //     2.2. display - Prints the elements of linked list from front to end in a single line. 
-//      All elements are separated by space.
-// 2.3. size - Returns the number of elements in the linked list.
-// 2.4. removeFirst - Removes the first element from Linked List. 
-// 3. You are required to complete the body of getFirst, getLast and getAt function 
-// 3.1. getFirst - Should return the data of first element. If empty should return -1 and print "List is empty".
-// 3.2. getLast - Should return the data of last element. If empty should return -1 and print "List is empty".
-// 3.3. getAt - Should return the data of element available at the index passed. If empty should return -1 and print "List is empty". If invalid index is passed, should return -1 and print "Invalid arguments".
-// 4. Input and Output is managed for you.  
+//     All elements are separated by space.
+//     2.3. size - Returns the number of elements in the linked list.
+//     2.4. removeFirst - Removes the first element from Linked List. 
+//     2.5. getFirst - Returns the data of first element. 
+//     2.6. getLast - Returns the data of last element. 
+//     2.7. getAt - Returns the data of element available at the index passed. 
+// 3. You are required to complete the body of addFirst function. This function should add the element to the beginning of the linkedlist and appropriately set the head, tail and size data-members.
+// 4. Input and Output is managed for you.
 
-public class Q89_GetValueInLinkedList {
+public class Q90_AddFirstInLinkedList {
     public static class Node {
         int data;
         Node next;
@@ -40,21 +40,21 @@ public class Q89_GetValueInLinkedList {
           size++;
         }
     
-        public int size(){
+        public int size() {
           return size;
         }
     
-        public void display(){
-          for(Node temp = head; temp != null; temp = temp.next){
+        public void display() {
+          for (Node temp = head; temp != null; temp = temp.next) {
             System.out.print(temp.data + " ");
           }
           System.out.println();
         }
     
-        public void removeFirst(){
-          if(size == 0){
+        public void removeFirst() {
+          if (size == 0) {
             System.out.println("List is empty");
-          } else if(size == 1){
+          } else if (size == 1) {
             head = tail = null;
             size = 0;
           } else {
@@ -63,53 +63,55 @@ public class Q89_GetValueInLinkedList {
           }
         }
     
-        public int getFirst(){
-          // write your code here
-          Node t=head;
-          if(t==null) {
+        public int getFirst() {
+          if (size == 0) {
             System.out.println("List is empty");
             return -1;
-          }else {
-            return t.data;
+          } else {
+            return head.data;
           }
         }
     
-        public int getLast(){
-          // write your code here
-          Node t=head;
-          if(t==null) {
+        public int getLast() {
+          if (size == 0) {
             System.out.println("List is empty");
             return -1;
+          } else {
+            return tail.data;
           }
-    
-          while(t.next!=null){
-            t=t.next;
-          }
-          return t.data;
-    
         }
     
-        public int getAt(int idx){
-          // write your code here
-          Node t=head;
-    
-          if(t==null) {
+        public int getAt(int idx) {
+          if (size == 0) {
             System.out.println("List is empty");
             return -1;
-          } 
-    
-          if(idx<0 || idx>=size) {
+          } else if (idx < 0 || idx >= size) {
             System.out.println("Invalid arguments");
             return -1;
+          } else {
+            Node temp = head;
+            for (int i = 0; i < idx; i++) {
+              temp = temp.next;
+            }
+            return temp.data;
+          }
+        }
+    
+        public void addFirst(int val) {
+          // write your code here
+          Node n=new Node();
+          n.data=val;
+          n.next=null;
+    
+          if(head==null) {
+            head=tail=n;
+            size++;
+            return;
           }
     
-          for(int i=0; i<idx; i++){
-            t=t.next;
-          }
-    
-          
-          return t.data;
-          
+          n.next=head;
+          head=n;
+          size++;
         }
       }
     
@@ -118,32 +120,35 @@ public class Q89_GetValueInLinkedList {
         LinkedList list = new LinkedList();
     
         String str = br.readLine();
-        while(str.equals("quit") == false){
-          if(str.startsWith("addLast")){
+        while (str.equals("quit") == false) {
+          if (str.startsWith("addLast")) {
             int val = Integer.parseInt(str.split(" ")[1]);
             list.addLast(val);
-          } else if(str.startsWith("size")){
+          } else if (str.startsWith("size")) {
             System.out.println(list.size());
-          } else if(str.startsWith("display")){
+          } else if (str.startsWith("display")) {
             list.display();
-          } else if(str.startsWith("removeFirst")){
+          } else if (str.startsWith("removeFirst")) {
             list.removeFirst();
-          } else if(str.startsWith("getFirst")){
+          } else if (str.startsWith("getFirst")) {
             int val = list.getFirst();
-            if(val != -1){
+            if (val != -1) {
               System.out.println(val);
             }
-          } else if(str.startsWith("getLast")){
+          } else if (str.startsWith("getLast")) {
             int val = list.getLast();
-            if(val != -1){
+            if (val != -1) {
               System.out.println(val);
             }
-          } else if(str.startsWith("getAt")){
+          } else if (str.startsWith("getAt")) {
             int idx = Integer.parseInt(str.split(" ")[1]);
             int val = list.getAt(idx);
-            if(val != -1){
+            if (val != -1) {
               System.out.println(val);
             }
+          } else if (str.startsWith("addFirst")) {
+            int val = Integer.parseInt(str.split(" ")[1]);
+            list.addFirst(val);
           }
           str = br.readLine();
         }
