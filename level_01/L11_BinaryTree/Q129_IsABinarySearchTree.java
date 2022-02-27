6eb00b1058b3a9e76075bc067cc309ba00e0950e
ayanluap <ayanluap@gmail.com>
2022-02-27 16:28:59 +0530

    BT still going on

--- level_01/L11_BinaryTree/Q129_IsABinarySearchTree.java
+++ level_01/L11_BinaryTree/Q129_IsABinarySearchTree.java
@@ -2,11 +2,17 @@ package L11_BinaryTree;
 import java.io.*;
 import java.util.*;
 
-// 1. You are given a partially written BinaryTree class. 
-// 2. You are required to complete the body of tilt function. The function is expected to set the value of data member "tilt". "tilt" of a node is the absolute value of difference between sum of nodes in it's left subtree and right subtree. "tilt" of the whole tree is represented as the sum of "tilt"s of all it's nodes. 
-// 3. Input and Output is managed for you. Note -> Please refer the video for clarity.
+// 1. You are given a partially written BinaryTree class.
+// 2. You are required to check if the tree is a Binary Search Tree (BST) as well. In a BST every node has a value greater than all nodes on it's left side and smaller value than all node on it's right side.
+// 3. Input is managed for you. 
 
-public class Q128_TiltOfBinaryTree {
+// Note -> Please refer the question video for clarity.
+
+// Constraints
+// Time complexity must be O(n)
+// Space should not be more than required for recursion (call-stack)
+
+public class Q129_IsABinarySearchTree {
     public static class Node {
         int data;
         Node left;
@@ -96,20 +102,43 @@ public class Q128_TiltOfBinaryTree {
         return th;
       }
     
-      static int tilt = 0;
+      public static class BSTPair {
+        boolean bst;
+        int max;
+        int min;
     
-      public static int tilt(Node node){
-        // write your code here to set the tilt data member
-        if(node==null) return 0;
+        BSTPair(){
     
-        int lsum= tilt(node.left); 
-        int rsum= tilt(node.right);
+        }
     
-        tilt+= Math.abs(lsum-rsum);
-    
-        return node.data + lsum + rsum;
+        BSTPair(boolean bst, int max, int min){
+          this.bst= bst;
+          this.max= max;
+          this.min= min;
+        }
       }
     
+    
+      public static BSTPair isBst(Node node){
+        if(node==null) {
+          BSTPair bres= new BSTPair(true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+          return bres;
+        }
+    
+        BSTPair lt= isBst(node.left);
+        BSTPair rt= isBst(node.right);
+    
+        BSTPair ct= new BSTPair();
+        if(lt.bst && rt.bst && node.data > lt.max && node.data < rt.min){
+          ct.bst=true;
+        } else ct.bst=false;
+        ct.max= Math.max(node.data, Math.max(lt.max, rt.max));
+        ct.min= Math.min(node.data, Math.min(lt.min, rt.min));
+    
+        return ct;
+      }
+     
+      
       public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -124,8 +153,8 @@ public class Q128_TiltOfBinaryTree {
         }
     
         Node root = construct(arr);
-    
-        tilt(root);
-        System.out.println(tilt);
+        
+        // write your code here
+        System.out.println(isBst(root).bst);
       }
 }
